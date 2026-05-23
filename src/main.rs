5f6eee8b6978b8ada40637f45b68bad576b95b42
Julian Zapata <julianandreszb@gmail.com>
2026-05-22 19:28:52 -0500

    Add initial Bevy setup with dynamic linking
    - Configured `Cargo.toml` to enable Bevy's dynamic linking feature.
    - Updated `Cargo.lock` with dependencies for `bevy_dylib`.
    - Implemented basic Bevy application with systems for greeting and updating `Person` components.
    - Added `.idea/db-forest-config.xml` for project configuration.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use bevy::prelude::*;
+
 fn main() {
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
+}
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Julian Zapata".to_string())));
+    commands.spawn((Person, Name("Ian Zapata".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0)
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Julian Zapata" {
+            name.0 = "Julian ZB".to_string();
+            break;
+        }
+    }
 }
